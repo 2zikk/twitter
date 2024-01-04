3e749bbb4a6941c3d0deb5d8260c8be7c7191f57
Hyesooo <hyesookim95@gmail.com>
2024-01-04 10:57:18 +0900

    :wrench: 환경 세팅

---
+++ src/test/java/com/stduy/social/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stduy.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
