ec7bba0e5ee29ac4ecf576a76260c38eeaac5c3e
배만진 <gentlemmj@gmail.com>
2024-01-08 17:23:49 +0900

    :sparkles: aop 기반 post 변경 감지 기능 추가

---
+++ src/main/java/com/stduy/social/adapter/in/event/PostEventListener.java
@@ -0,0 +1,18 @@
+package com.stduy.social.adapter.in.event;
+
+import com.stduy.social.domain.PostCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(PostEventListener.class);
+
+    @EventListener
+    public void onPostCreated(PostCreatedEvent event) {
+        logger.info("receive post event: {}", event.getPostId());
+    }
+}
